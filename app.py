6fb29b4819138961da018703c1c8312e131b1e66
Nikhil Dehal <23mcs10044@cuchd.in>
2024-11-07 09:59:54 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,55 @@
+import streamlit as st
+import pickle
+import pandas as pd
+import numpy as np
+
+
+def recommend(movie):
+    movie_index = movies[movies['title'] == movie].index[0]
+    distance = similarity[movie_index]
+    movie_list = sorted(list(enumerate(distance)), reverse=True, key=lambda x: x[1])[1:6]
+    recommended_movies = []
+    for i in movie_list:
+        recommended_movies.append(movies.iloc[i[0]].title)
+    return recommended_movies
+
+
+movies_dict = pickle.load(open('movie_dict.pkl', 'rb'))
+movies = pd.DataFrame(movies_dict)
+#
+# from joblib import load
+#
+# similarity = load('similarity.joblib')
+
+similarity = pickle.load(open('similarity.pkl', 'rb'))
+
+st.title("Recommender")
+selected_movie_name = st.selectbox('Select a movie:', movies['title'].values)
+
+if st.button('Recommend'):
+    recommendations = recommend(selected_movie_name)
+    for i in recommendations:
+        st.write(i)
+
+theme = st.radio("Choose theme", options=["Light", "Dark"])
+
+if theme == "Dark":
+    st.markdown(
+        """
+        <style>
+            .css-18e3th9 {
+                background-color: #0E1117;
+                color: #FFFFFF;
+            }
+        </style>
+        """, unsafe_allow_html=True)
+else:
+    st.markdown(
+        """
+        <style>
+            .css-18e3th9 {
+                background-color: #FFFFFF;
+                color: #000000;
+            }
+        </style>
+        """, unsafe_allow_html=True)
